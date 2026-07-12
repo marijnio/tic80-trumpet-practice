e0347ad982535f9caf74be2dc9b342b272c67eac
Marijn <np273974@politie.nl>
2026-07-12 22:48:34 +0200

    Fixes pitch of trumpet sounds

--- trumpet-practice.lua
+++ trumpet-practice.lua
@@ -163,13 +163,13 @@ end
 local function sfx(id, channel)
   if id == 0 then
     -- Success chime: E-5
-    _sfx(0, 76, 15, channel or 1, 12, 0)
+    _sfx(0, 52, 15, channel or 1, 12, 0)
   elseif id == 1 then
     -- Failure chime: C-3
-    _sfx(0, 48, 20, channel or 1, 12, 0)
+    _sfx(0, 24, 20, channel or 1, 12, 0)
   elseif id == 4 then
     -- Click metronome: C-6
-    _sfx(0, 84, 2, channel or 2, 10, 0)
+    _sfx(0, 60, 2, channel or 2, 10, 0)
   elseif id == -1 then
     -- Stop sound
     _sfx(-1, 0, 0, channel or 3)
@@ -178,8 +178,8 @@ end
 
 -- Play drone note
 function play_pitch(p)
-  local midi_note = p + 36
-  _sfx(0, midi_note, -1, 3, 12, 0)
+  local midi_note = p + 12
+  _sfx(1, midi_note, -1, 3, 12, 0)
 end
 
 function stop_pitch()
@@ -1009,3 +1009,33 @@ function TIC()
   _update()
   _draw()
 end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- 001:020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
